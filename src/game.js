831af92098eb03a5688b172733008023df45bd51
Taran Cacacho <tarancacacho@gmail.com>
2019-07-20 14:52:38 -0700

    Flip default dimensions, pass in instance of Game to each instantiation of Asteroid, and write #wrap

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 const Asteroid = require('./asteroids');
 
 DEFAULTS = {
-  DIM_X: 600,
-  DIM_Y: 800,
+  DIM_X: 800,
+  DIM_Y: 600,
   NUM_ASTEROIDS: 20
 }
 
@@ -13,7 +13,7 @@ function Game() {
 
 Game.prototype.addAsteroids = function() {
   for (let i = 0; i < DEFAULTS.NUM_ASTEROIDS; i++) {
-    let ast = new Asteroid({pos: this.randomPosition()});
+    let ast = new Asteroid({pos: this.randomPosition(), game: this});
     this.asteroids.push(ast);
   }
 }
@@ -31,6 +31,23 @@ Game.prototype.draw = function(ctx) {
 
 Game.prototype.moveObjects = function() {
   this.asteroids.forEach(ast => ast.move());
+}
+
+Game.prototype.wrap = function(pos) {
+  let newPos = [pos[0], pos[1]];
+  if (pos[0] > DEFAULTS.DIM_X) {
+    newPos[0] = newPos[0] % DEFAULTS.DIM_X;
+  } else if (pos[0] < 0) {
+    newPos[0] = newPos[0] + DEFAULTS.DIM_X;
+  }
+
+  if (pos[1] > DEFAULTS.DIM_Y) {
+    newPos[1] = newPos[1] % DEFAULTS.DIM_Y;
+  } else if (pos[1] < 0) {
+    newPos[1] = newPos[1] + DEFAULTS.DIM_Y;
+  }
+
+  return newPos;
 }
 
 module.exports = Game;
