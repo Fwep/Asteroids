a2611b56688f81627a8a25e2f797da29612dda7b
Taran Cacacho <tarancacacho@gmail.com>
2019-07-19 23:41:07 -0700

    Add functions to return random vectors, create inheritance structures, and scale vectors

--- src/util.js
+++ src/util.js
@@ -1,6 +1,15 @@
 const Util = {
-  inherits: function inherits(childClass, ParentClass) {
-    childClass.prototype = Object.create(ParentClass.prototype);
-    childClass.prototype.constructor = childClass;
+  inherits: function inherits(ChildClass, ParentClass) {
+    ChildClass.prototype = Object.create(ParentClass.prototype);
+    ChildClass.prototype.constructor = ChildClass;
+  },
+  randomVec: function randomVec(length) {
+    const deg = 2 * Math.PI * Math.random();
+    return Util.scale([Math.sin(deg), Math.cos(deg)], length);
+  },
+  scale: function(vec, m) {
+    return [vec[0] * m, vec[1] * m];
   }
 }
+
+module.exports = Util;
