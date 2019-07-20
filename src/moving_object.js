16d41a37f8113b202b47135d70561103f435a5e5
Taran Cacacho <tarancacacho@gmail.com>
2019-07-20 14:53:58 -0700

    Save instance of game in constructor, set fillStyle to moving object's color attribute, and wrap objects around canvas as they move

--- src/moving_object.js
+++ src/moving_object.js
@@ -3,10 +3,11 @@ function MovingObject(settings) {
   this.vel = settings.vel;
   this.radius = settings.radius;
   this.color = settings.color;
+  this.game = settings.game;
 }
 
 MovingObject.prototype.draw = function(ctx) {
-  ctx.fillStyle = 'blue';
+  ctx.fillStyle = this.color;
   ctx.beginPath();
   let [x, y] = [this.pos[0], this.pos[1]];
   ctx.arc(x, y, this.radius, 0, 2 * Math.PI);
@@ -17,7 +18,7 @@ MovingObject.prototype.move = function() {
   let [posX, posY] = [this.pos[0], this.pos[1]];
   let [velX, velY] = [this.vel[0], this.vel[1]];
   let newPos =  [posX + velX, posY + velY];
-  this.pos = newPos;
+  this.pos = this.game.wrap(newPos);
 }
 
 module.exports = MovingObject;
