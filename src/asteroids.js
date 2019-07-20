000d42990eec358a8839f4d496a0a98248edb5cd
Taran Cacacho <tarancacacho@gmail.com>
2019-07-19 23:42:45 -0700

    Create Asteroid subclass of MovingObject, set default attribute values

---
+++ src/asteroids.js
@@ -0,0 +1,20 @@
+const MovingObject = require('./moving_object');
+const Util = require('./util');
+
+const DEFAULTS = {
+  COLOR: 'rgb(46.4%, 36.1%, 20.4%)',
+  RADIUS: 80
+}
+
+function Asteroid(options) {
+  options.color = DEFAULTS.COLOR;
+  options.radius = DEFAULTS.RADIUS; 
+  options.vel = Util.randomVec(2);
+
+  MovingObject.call(this, options);
+}
+
+Util.inherits(Asteroid, MovingObject);
+
+
+module.exports = Asteroid;
