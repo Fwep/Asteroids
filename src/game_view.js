48549d3648987a500bfa23351b170cf342edadae
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 00:04:26 -0700

    Utilize keymaster.js to bind controls to fly and shoot

--- src/game_view.js
+++ src/game_view.js
@@ -1,11 +1,27 @@
+const Ship = require('./ship')
+
 function GameView(ctx) {
   this.game = new Game;
   this.ctx = ctx;
 }
 
+GameView.prototype.bindKeyHandlers = function () {
+  // keymaster exposes a global method, key(key, callback) that triggers the callback when the key is pressed
+  let {ship} = this.game;
+  key('w', () => {ship.power([0,-1])});
+  key('a', () => {ship.power([-1,0])});
+  key('s', () => {ship.power([0,1])});
+  key('d', () => {ship.power([1,0])});
+  key('space', () => {
+    ship.fireBullet()
+    return false;
+  });
+}
+
 GameView.prototype.start = function() {
+  this.bindKeyHandlers();
   window.setInterval(() => {
-    this.game.moveObjects(ctx);
+    this.game.step(this.ctx);
     this.game.draw(this.ctx);
   }, 20);
 }
