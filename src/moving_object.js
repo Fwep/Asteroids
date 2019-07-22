3d8eaf0f4f17a2afad41647b47ef4696463e65c5
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 00:00:36 -0700

    Modify MovingObject to only wrap non-bullet moving objects

--- src/moving_object.js
+++ src/moving_object.js
@@ -1,3 +1,5 @@
+const Util = require('./util');
+
 function MovingObject(settings) {
   this.pos = settings.pos;
   this.vel = settings.vel;
@@ -6,19 +8,39 @@ function MovingObject(settings) {
   this.game = settings.game;
 }
 
+MovingObject.prototype.collideWith = function(otherObject) {}
+
 MovingObject.prototype.draw = function(ctx) {
   ctx.fillStyle = this.color;
   ctx.beginPath();
   let [x, y] = [this.pos[0], this.pos[1]];
   ctx.arc(x, y, this.radius, 0, 2 * Math.PI);
   ctx.fill();
+}  
+
+MovingObject.prototype.isCollidedWith = function(otherObject) {
+  let sumRadii = otherObject.radius + this.radius;
+  return Util.distance(this.pos, otherObject.pos) < sumRadii;
 }
+
+MovingObject.prototype.isWrappable = true;
 
 MovingObject.prototype.move = function() {
   let [posX, posY] = [this.pos[0], this.pos[1]];
   let [velX, velY] = [this.vel[0], this.vel[1]];
   let newPos =  [posX + velX, posY + velY];
-  this.pos = this.game.wrap(newPos);
-}
+
+  if (this.game.isOutOfBounds(newPos)) {
+    if (this.isWrappable) {
+      this.pos = this.game.wrap(newPos);
+      return;
+    } else {
+      this.game.remove(this);
+      return;
+    }
+  } else {
+    this.pos = newPos;
+  }
+}  
 
 module.exports = MovingObject;
