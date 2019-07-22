0cc65f6d7ddb7010d7d8306878360e363fd2bbd5
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 00:04:06 -0700

    Add utility functions to return normalized vector, and measure distance between two objects

--- src/util.js
+++ src/util.js
@@ -1,4 +1,12 @@
 const Util = {
+  direction: function direction(vec) {
+    if (Math.abs(vec[0]) > 1) vec[0] /= vec[0];
+    if (Math.abs(vec[1]) > 1) vec[1] /= vec[1];
+    return vec;
+  },
+  distance: function distance(p1, p2) {;
+    return Math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2));
+  },
   inherits: function inherits(ChildClass, ParentClass) {
     ChildClass.prototype = Object.create(ParentClass.prototype);
     ChildClass.prototype.constructor = ChildClass;
