680d2127d1c5c27471c65e1e0444644d9b7605ae
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 00:03:47 -0700

    Add instance methods to relocate, fire bullets, and move

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,6 @@
 const Util = require('./util');
 const MovingObject = require('./moving_object');
+const Bullet = require('./bullet');
 
 function Ship(options) {
   options.vel = [0,0];
@@ -9,9 +10,25 @@ function Ship(options) {
   MovingObject.call(this, options);
 }
 
+Util.inherits(Ship, MovingObject);
+
+Ship.prototype.fireBullet = function() {
+  let bullet = new Bullet({vel: this.vel.slice(), pos: this.pos, game: this.game});
+  this.game.add(bullet);
+}
+
+Ship.prototype.relocate = function() {
+  this.pos = this.game.randomPosition();
+  this.vel = [0, 0];
+}
+
+Ship.prototype.power = function(impulse) {
+  let newVel = [this.vel[0] + impulse[0], this.vel[1] + impulse[1]];
+  this.vel = newVel;  
+}
+
 Ship.RADIUS = 20;
 Ship.COLOR = 'rgb(22.9%, 20.4%, 46.4%)';
 
-Util.inherits(Ship, MovingObject);
 
 module.exports = Ship;
