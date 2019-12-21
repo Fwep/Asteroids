c78e59554328cdde3393004fcd2e27e6bdbd14f3
Taran Cacacho <tarancacacho@gmail.com>
2019-12-21 02:31:55 -0800

    Type Ship class, refactor for ES6+ syntax

--- src/ship.js
+++ src/ship.js
@@ -1,20 +1,40 @@
-const Util = require('./util');
-const MovingObject = require('./moving_object');
-const Bullet = require('./bullet');
+// @flow
+import Util from './util';
+import {type Options} from './moving_object';
+import {MovingObject} from './moving_object';
+import Bullet from './bullet';
 
-function Ship(options) {
-  options.vel = [0,0];
-  options.radius = Ship.RADIUS;
-  options.color = Ship.COLOR;
+export default class Ship<T: Options> extends MovingObject {
+  static RADIUS: number;
+  static COLOR: string;
 
-  MovingObject.call(this, options);
-}
+  constructor(options: T) {
+    options.vel = [0, 0];
+    options.radius = Ship.RADIUS;
+    options.color = Ship.COLOR;
+    options.isWrappable = true;
 
-Util.inherits(Ship, MovingObject);
+    super(options);
+  }
 
-Ship.prototype.fireBullet = function() {
-  let bullet = new Bullet({vel: this.vel.slice(), pos: this.pos, game: this.game});
-  this.game.add(bullet);
+  fireBullet() {
+    let bullet = new Bullet({
+      vel: this.vel.slice(),
+      pos: this.pos,
+      game: this.game
+    });
+    this.game.add(bullet);
+  }
+
+  power(impulse: [number, number]): void {
+    let newVel = [this.vel[0] + impulse[0], this.vel[1] + impulse[1]];
+    this.vel = newVel;  
+  }
+
+  relocate() {
+    this.pos = this.game.randomPosition();
+    this.vel = [0, 0];
+  }
 }
 
 Ship.prototype.relocate = function() {
@@ -22,13 +42,5 @@ Ship.prototype.relocate = function() {
   this.vel = [0, 0];
 }
 
-Ship.prototype.power = function(impulse) {
-  let newVel = [this.vel[0] + impulse[0], this.vel[1] + impulse[1]];
-  this.vel = newVel;  
-}
-
 Ship.RADIUS = 20;
 Ship.COLOR = 'rgb(22.9%, 20.4%, 46.4%)';
-
-
-module.exports = Ship;
